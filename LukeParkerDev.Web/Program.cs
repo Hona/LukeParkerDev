fe1cec1400339816aa50e604fa0d15408e8e88de
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2021-10-29 09:36:23 +1000

    ✨ [WIP]

---
+++ LukeParkerDev.Web/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using LukeParkerDev.Web;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var services = builder.Services;
+
+services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+services.AddMudServices();
+
+await builder.Build().RunAsync();
